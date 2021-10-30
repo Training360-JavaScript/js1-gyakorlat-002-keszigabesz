4afdb08c76cb0e7add63a063afb8bb008c2c5b76
keszigabesz <bajo.gabor@gmail.com>
2021-10-30 11:42:11 +0200

    kész van, de a teszt hibát jelez

--- js/first.js
+++ js/first.js
@@ -4,3 +4,8 @@
 - A függvény szorozza meg a tömbelemek értékét 1.27-tel, majd pedig 
 __térjen vissza__ az elemek egész számra kerekített összegével!
 */
+const exampleArray = [12, 34, 56, 78];
+const brutto = function (array){
+    return array.map(item=>Math.round(item*1.27)).reduce((previousValue, currentValue)=>previousValue+currentValue);
+}
+brutto(exampleArray);
--- js/main.js
+++ js/main.js
@@ -3,4 +3,31 @@
 - Írj egy olyan függvénykifejezést, amely paraméterként egy string-eket tartalmazó tömböt kap (inputellenőrzést nem kell végezni), és __visszaad__ egy HTML-template-et (string)! 
 - A függvény neve `generateList` legyen!
 - A HTML-template egy lista, amelynek a listaelemei a kapott tömb értékeit tartalmazzák.
+ 
+```JavaScript
+// ha ez a tömb
+const content = ['első', 'második', 'harmadik'];
+```
+
+```HTML
+<!-- akkor ez legyen a visszatérési érték (nem kellenek a behúzások, lehet egysoros string a kimenet): -->
+<ul>
+    <li>első</li>
+    <li>második</li>
+    <li>harmadik</li>
+</ul>
+```
 */
+
+const content = ['első', 'második', 'harmadik'];
+
+const generateList = function (list) {
+    let kimenet = "<ul>";
+    list.forEach(item => {
+        kimenet += "<li>"+item+"</li>";
+    });
+    kimenet +="</ul>";
+    console.log(kimenet);
+}
+
+console.log(generateList(content));
